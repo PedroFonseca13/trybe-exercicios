267cb19e9f25b1c2ce035cda10a2454d773755e4
Pedro Fonseca <pedrinhopedro01@gmail.com>
2021-08-25 16:50:36 -0300

    arquivo relacionado aos primeiros passos de JS

---
+++ Fundamentos/bloco4/dia1-JS-primeiros-passos/script.js
@@ -0,0 +1,36 @@
+// variaveis;
+const a = 10;
+const b = 5;
+
+// operadores aritméticos;
+let adicao = a + b;
+let subtracao = a - b;
+let multiplicacao = a * b;
+let divisao = a / b;
+let modulo = a % b;
+
+console.log(adicao)
+console.log(subtracao)
+console.log(multiplicacao)
+console.log(divisao)
+console.log(modulo)
+
+// exercicio 2
+const num1 = 1300; 
+const num2 = 420;
+const num3 = 92;
+
+if (num1 > num2) {
+  console.log(num1)
+} else {
+  console.log(num2)
+}
+
+// exercicio 3
+if (num1 > num2 && num1 > num3) {
+  console.log(num1)
+} else if (num2 > num1 && num2 > num3) {
+  console.log(num2)
+} else if (num3 > num1 && num3 > num2) {
+  console.log(num3)
+}
