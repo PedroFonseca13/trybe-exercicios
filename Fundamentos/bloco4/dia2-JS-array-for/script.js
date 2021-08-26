6dd5f69e5351776a3128105af41ba54f7f7ee544
Pedro Fonseca <pedrinhopedro01@gmail.com>
2021-08-26 19:37:12 -0300

    Exercicio 06 feito de maneira correta

--- Fundamentos/bloco4/dia2-JS-array-for/script.js
+++ Fundamentos/bloco4/dia2-JS-array-for/script.js
@@ -1,16 +1,16 @@
-let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27, 420];
-let sum = 0;
-let median = 0;
-let maxNumber = 0;
-let even = 0;
+let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
+let sum = 0
+let median = 0
+let maxNumber = 0
+let even = 0
 
 // 01 - Nesse primeiro exercício, percorra o array imprimindo todos os valores nele contidos com a função console.log() ;
 
 for (let index = 0; index < numbers.length; index++) {
-  console.log(numbers[index]);
+  console.log(numbers[index])
 }
 
-console.log("----------------------------------------------------------")
+console.log('----------------------------------------------------------')
 
 // 02 - Para o segundo exercício, some todos os valores contidos no array e imprima o resultado;
 
@@ -18,9 +18,9 @@ for (let i = 0; i < numbers.length; i++) {
   sum += numbers[i]
 }
 
-console.log(`Soma total dos valores contidos no array é ${sum}`);
+console.log(`Soma total dos valores contidos no array é ${sum}`)
 
-console.log("----------------------------------------------------------")
+console.log('----------------------------------------------------------')
 
 // 03 - Para o terceiro exercício, calcule e imprima a média aritmética dos valores contidos no array;
 
@@ -29,43 +29,50 @@ for (let i = 0; i < numbers.length; i++) {
   median = sum / numbers.length
 }
 
-console.log(`Média aritmética dos valores contidos no array é ${median.toFixed(2)}`);
+console.log(
+  `Média aritmética dos valores contidos no array é ${median.toFixed(2)}`
+)
 
-console.log("----------------------------------------------------------")
+console.log('----------------------------------------------------------')
 
 // 04 - Com o mesmo código do exercício anterior, caso o valor final seja maior que 20, imprima a mensagem: "valor maior que 20". Caso não seja, imprima a mensagem: "valor menor ou igual a 20";
 
 for (let i = 0; i < numbers.length; i++) {
   sum += numbers[i]
   median = sum / numbers.length
-  
 }
 
 if (median > 20) {
-  console.log(`Valor maior que 20`);
+  console.log(`Valor maior que 20`)
 } else {
   console.log(`Valor menor que 20`)
 }
 
-console.log("----------------------------------------------------------")
+console.log('----------------------------------------------------------')
 
 // 05 - Utilizando for , descubra qual o maior valor contido no array e imprima-o;
 for (let i = 0; i < numbers.length; i++) {
-  if(numbers[i] > maxNumber) {
-    maxNumber = numbers[i];
+  if (numbers[i] > maxNumber) {
+    maxNumber = numbers[i]
   }
 }
 
-console.log(`O maior valor contindo no array é ${maxNumber}`);
+console.log(`O maior valor contindo no array é ${maxNumber}`)
 
-console.log("----------------------------------------------------------")
+console.log('----------------------------------------------------------')
 
 // 06 - Descubra quantos valores ímpares existem no array e imprima o resultado. Caso não exista nenhum, imprima a mensagem: "nenhum valor ímpar encontrado";
 
 for (let i = 0; i < numbers.length; i++) {
-  if ((numbers[i] % 2) != 0) {
-    even ++
+  if (numbers[i] % 2 != 0) {
+    even++
   }
 }
 
-console.log(even);
+if (even == 0) {
+  console.log('Nenhum valor ímpar encontrado')
+} else {
+  console.log(`${even} valores ímpares econtrado(s)`)
+}
+
+console.log('----------------------------------------------------------')
