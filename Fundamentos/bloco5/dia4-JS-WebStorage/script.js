43172a4273b4303f5d96da4a70bcc3fc05b65fed
Pedro Fonseca <pedrinhopedro01@gmail.com>
2021-09-10 14:21:19 -0300

    pequenas mudancas no documento

--- Fundamentos/bloco5/dia4-JS-WebStorage/script.js
+++ Fundamentos/bloco5/dia4-JS-WebStorage/script.js
@@ -1,36 +1,46 @@
 window.onload = () => {
   let sectionExample = document.querySelector('section#example')
 
+  // Muda o background
   function changeBackground() {
     let select = document.querySelector('select#select-background')
     select.addEventListener('change', () => {
       let selected = select.selectedOptions[0]
       sectionExample.style.backgroundColor = selected.value
+
+      localStorage.setItem('select-background', selected.value)
     })
   }
 
   changeBackground()
 
+  // muda a cor do texto
   function changeTextColor() {
     let selectColor = document.querySelector('select#select-color')
     selectColor.addEventListener('change', () => {
       let colorSelected = selectColor.selectedOptions[0]
       sectionExample.style.color = colorSelected.value
+
+      localStorage.setItem('select-color', colorSelected.value)
     })
   }
 
   changeTextColor()
 
+  // muda o Font size
   function changeFontSize() {
     let selectFontSize = document.querySelector('select#select-FontSize')
     selectFontSize.addEventListener('change', () => {
       let fontSize = selectFontSize.selectedOptions[0]
       sectionExample.style.fontSize = `${Number(fontSize.value)}rem`
+
+      localStorage.setItem('selectFontSize', fontSize.value)
     })
   }
 
   changeFontSize()
 
+  // muda o line height
   function changeLineHeight() {
     let selectLineHeight = document.querySelector('select#select-LineHeight')
     selectLineHeight.addEventListener('change', () => {
@@ -41,6 +51,7 @@ window.onload = () => {
 
   changeLineHeight()
 
+  // muda a fontFamily
   function changeFontFamily() {
     let selectFontFamily = document.querySelector('select#select-FontFamily')
     selectFontFamily.addEventListener('change', () => {
@@ -50,4 +61,16 @@ window.onload = () => {
   }
 
   changeFontFamily()
+
+  function savePreferences() {
+    let savedBG = localStorage.getItem('select-background')
+    sectionExample.style.backgroundColor = savedBG
+    
+    let savedColor = localStorage.getItem('select-color')
+    sectionExample.style.color = savedColor
+
+    let savedFS = localStorage.getItem('select-Font')
+  }
+
+  savePreferences()
 }
