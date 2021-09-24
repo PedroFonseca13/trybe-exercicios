7abef140d8d99bb1fba9b3ded47c3212c64efd2a
Pedro Fonseca <pedrinhopedro01@gmail.com>
2021-09-24 19:25:53 -0300

    parte 2 do exercicio proposto resolvido

--- Fundamentos/bloco7/dia01-JS-ES6/script.js
+++ Fundamentos/bloco7/dia01-JS-ES6/script.js
@@ -1,13 +1,31 @@
-const testingScope = (escopo) => {
-  if (scope === true) {
-    let ifScope = `Não devo ser utilizada fora do meu escopo (if)`;
-    ifScope = ifScope + ' ótimo, fui utilizada no escopo !';
-    console.log(ifScope);
-  } else {
-    const elseScope = 'Não devo ser utilizada fora meu escopo (else)';
-    console.log(elseScope);
-  }
-  console.log(ifScope + ' o que estou fazendo aqui ? :O'); // Se necessário esta linha pode ser removida.
+// const testingScope = (escopo) => {
+//   if (scope === true) {
+//     let ifScope = `Não devo ser utilizada fora do meu escopo (if)`;
+//     ifScope = `${ifScope} ótimo, fui utilizada no escopo !`;
+//     console.log(ifScope);
+//   } else {
+//     const elseScope = 'Não devo ser utilizada fora meu escopo (else)';
+//     console.log(elseScope);
+//   }
+//   console.log(ifScope + ' o que estou fazendo aqui ? :O'); // Se necessário esta linha pode ser removida.
+// }
+
+// testingScope(true);
+
+
+// ex B
+const oddsAndEvens = [13, 3, 4, 10, 7, 2];
+
+const sortOddsAndEvens = () => {
+  oddsAndEvens[0] = 2;
+  oddsAndEvens[1] = 3;
+  oddsAndEvens[2] = 4;
+  oddsAndEvens[3] = 7;
+  oddsAndEvens[4] = 10;
+  oddsAndEvens[5] = 13;
+
+  return oddsAndEvens;
 }
 
-testingScope(true);
+const sortedArray = sortOddsAndEvens()
+console.log(`Os números ${sortedArray} se encontram ordenados de forma crescente`); 
