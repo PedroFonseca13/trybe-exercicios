a771e1c909c8af9569c6a3e433814e13d413a7c6
Pedro Fonseca <pedrinhopedro01@gmail.com>
2021-09-27 21:37:18 -0300

    resolvido ate o requisito 3- do segundo exercicio proposto

---
+++ Fundamentos/bloco7/dia02-JS-Objects/parte2.js
@@ -0,0 +1,31 @@
+const lesson1 = {
+  materia: 'Matemática',
+  numeroEstudantes: 20,
+  professor: 'Maria Clara',
+  turno: 'manhã',
+}
+
+const lesson2 = {
+  materia: 'História',
+  numeroEstudantes: 20,
+  professor: 'Carlos',
+}
+
+const lesson3 = {
+  materia: 'Matemática',
+  numeroEstudantes: 10,
+  professor: 'Maria Clara',
+  turno: 'noite',
+}
+
+const addNewKey = (obj, key, value) => {
+  obj[key] = value
+}
+
+addNewKey(lesson2, 'turno', 'noite')
+
+const listKeys = obj => Object.keys(obj)
+console.log(listKeys(lesson1))
+
+const sizeObj = obj => Object.keys(obj).length
+log(sizeObj(lesson1))
