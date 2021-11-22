3e4017b53620b8bcc8f5d6aa258413c73d53b790
Pedro Fonseca <pedrinhopedro01@gmail.com>
2021-11-22 16:13:14 -0300

    arquivos do segundo exercicio

---
+++ front-end/bloco-10/dia-1-HelloWorld-react/app2/src/App.js
@@ -0,0 +1,19 @@
+import React, { Component } from 'react'
+import Header from './components/Header'
+import Content from './components/Content'
+
+import './App.css'
+import Footer from './components/Footer'
+
+export default class App extends Component {
+  render() {
+    return (
+      <>
+      <Header />
+      <Content />
+      <Footer />
+      </>
+    )
+  }
+}
+
---
+++ front-end/bloco-10/dia-1-HelloWorld-react/app2/src/components/Footer.jsx
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+
+export default class Footer extends Component {
+  render() {
+    return (
+      <footer className="footer-container">
+        <h1>E isso é so o começo...</h1>
+      </footer>
+    )
+  }
+}
